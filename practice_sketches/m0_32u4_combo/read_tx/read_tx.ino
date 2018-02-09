161d08a4cfbe71bd57717243ce1d9d062c77b5b4
CelticChristoph <celticchristoph@live.com>
2018-02-09 12:59:02 -0800

    Some testing with OSC and bundle managament, as well as LoRa communciation.

--- practice_sketches/m0_32u4_combo/read_tx/read_tx.ino
+++ practice_sketches/m0_32u4_combo/read_tx/read_tx.ino
@@ -12,6 +12,12 @@ SDI12 mySDI12(DATAPIN);
 
 String sdiResponse = "";
 String myCommand = "";
+char response_buff[30];
+char *pntr_for_resp;
+
+float dielec_p = 0;
+float temp = 0;
+float elec_c = 0;
 
 /* for feather32u4
   #define RFM95_CS 8
@@ -121,7 +127,55 @@ void loop()
       delay(5);
     }
   }
+//  sdiResponse.toCharArray(response_buff, sizeof(response_buff));
+  
   if (sdiResponse.length() > 1) Serial.println(sdiResponse); //write the response to the screen
+
+  Serial.println("Prep LoRa package...");
+
+  Serial.print("Length of response is: "); Serial.println(sizeof(sdiResponse));
+
+  strcpy(response_buff, sdiResponse.c_str());
+
+//  pntr_for_resp = response_buff
+//  strtok_r(p, "+", &p);
+//  dielec_p = atof(strtok_r(NULL, "+", &p));
+//  temp = atof(strtok_r(NULL, "+", &p));
+//  elec_c = atof(strtok_r(NULL, "+", &p));
+  
+  Serial.println("Send LoRa package...");
+
+  Serial.println("Sending..."); delay(10);
+  rf95.send((uint8_t *)response_buff, 30);
+
+  Serial.println("Waiting for packet to complete..."); delay(10);
+  rf95.waitPacketSent();
+  // Now wait for a reply
+  uint8_t return_buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(return_buf);
+
+  Serial.println("Waiting for reply..."); delay(10);
+  if (rf95.waitAvailableTimeout(1000))
+  {
+    // Should be a reply message for us now
+    if (rf95.recv(return_buf, &len))
+    {
+      Serial.print("Got reply: ");
+      Serial.println((char*)return_buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
+  else
+  {
+    Serial.println("No reply, is there a listener around?");
+  }
+  delay(1000); 
+  
   mySDI12.clearBuffer();
   sdiResponse = "";
 
