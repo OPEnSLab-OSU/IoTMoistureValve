dff0603be5241b1aed49888f3723682c4540e253
CelticChristoph <celticchristoph@live.com>
2018-03-09 15:23:39 -0800

    Funtioning multi-directoinal communication, uploads data, retrieves instructions.

--- src/hub/Hub_V0.2/Hub_V0.2.ino
+++ src/hub/Hub_V0.2/Hub_V0.2.ino
@@ -1,25 +1,25 @@
 /***********************************
- * Author: Peter Dorich 
- * Based off code from OSC by Kenny Noble 
- * Back-end for send/ recieve data between hub/ valve
+   Author: Peter Dorich
+   Based off code from OSC by Kenny Noble
+   Back-end for send/ recieve data between hub/ valve
  ************************************/
 #include <SPI.h>
 #include <RH_RF95.h>
 #include <RHReliableDatagram.h>
 #include <OSCBundle.h>
 
-/* for feather32u4 
+/* for feather32u4
   #define RFM95_CS 8
   #define RFM95_RST 4
   #define RFM95_INT 7
 */
 /* for M0 */
-  #define RFM95_CS 8
-  #define RFM95_RST 4
-  #define RFM95_INT 3
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
 #define LED 13
 
-  
+
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 
@@ -28,16 +28,30 @@
 #include <Dns.h>
 #include <Dhcp.h>
 
-#define SERVER_ADDRESS 1
-#define CLIENT_ADDRESS 2
+#define HUB_ADDRESS 1
+#define RELAY_ADDRESS 2
 
 // Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 915.0
 
+//Size of message for LoRa
+#define MSG_SIZE 121
+
+//IDString constructor
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x) //to concatenate a predefined number to a string literal, use STR(x)
+
+#define FAMILY "/LOOM"
+#define DEVICE "/Hub/"
+#define INSTANCE_NUM 0  // Unique instance number for this device, useful when using more than one of the same device type in same space
+
+#define IDString FAMILY DEVICE STR(INSTANCE_NUM) // C interprets subsequent string literals as concatenation: "/Loom" "/Ishield" "0" becomes "/Loom/Ishield0"
+
+
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
-RHReliableDatagram manager(rf95, SERVER_ADDRESS);
+RHReliableDatagram manager(rf95, HUB_ADDRESS);
 uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
 
 /********************Ethernet Client Setup******************************/
@@ -64,13 +78,14 @@ Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt,  AIO_USERNA
 
 void setup() {
   Serial.begin(9600);
-
+  while(!Serial);
+  
   //manual reset
   digitalWrite(RFM95_RST, LOW);
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
-  
+
   if (!manager.init())
     Serial.println("init failed");
 
@@ -89,84 +104,88 @@ void setup() {
   MQTT_connect();
 }
 
-uint32_t x=0;
+uint32_t x = 0;
 
 struct soil_data
 {
-    float VWC;
-    float TEMP;
-    uint32_t ELEC_COND;
-  
+  float VWC;
+  float TEMP;
+  uint32_t ELEC_COND;
+
 };
 
 soil_data s_dat;
+
+OSCBundle inst_bndl;
+
 void loop() {
   // put your main c here, to run repeatedly:l
- // soil_data f_dat;
- 
- //MQTT_connect();
-  
- // delay(5 * 1000);
-  //Adafruit_MQTT_Subscribe *subscription;
- /* while ((subscription = mqtt.readSubscription(1000))) {
-    if (subscription == &onoffbutton) {
-      Serial.print(F("Got: "));
-      Serial.println((char *)onoffbutton.lastread);
-    }
-  if (strcmp((char *)onoffbutton.lastread, "ON") == 0) {
-        digitalWrite(LED, HIGH); 
-      }
-  if (strcmp((char *)onoffbutton.lastread, "OFF") == 0) {
-        digitalWrite(LED, LOW); 
-      }
-  
-  }
-  */
-/*
-  s_dat.ELEC_COND = 25;
-  s_dat.TEMP = 34.5;
-  s_dat.VWC = 10.21;
-*/
+  // soil_data f_dat;
 
- // Elec_Cond.publish(s_dat.ELEC_COND, DEC);
-/*  if (! Elec_Cond.publish(s_dat.ELEC_COND)) {
-     Serial.println(F("Failed"));
-     } else {
-        Serial.println(F("Got Elec_Cond"));
+  //MQTT_connect();
+
+  // delay(5 * 1000);
+  //Adafruit_MQTT_Subscribe *subscription;
+  /* while ((subscription = mqtt.readSubscription(1000))) {
+     if (subscription == &onoffbutton) {
+       Serial.print(F("Got: "));
+       Serial.println((char *)onoffbutton.lastread);
      }
-  if (! Temperature.publish((char *) String(s_dat.TEMP).c_str())) {
-     Serial.println(F("Failed"));
-     } else {
-        Serial.println(F("Got Temp"));
-     }
-  if (! VWC.publish((char *) String(s_dat.VWC).c_str())) {
-     Serial.println(F("Failed"));
-     } else {
-        Serial.println(F("Got VWC"));
-     }
-  */   
+    if (strcmp((char *)onoffbutton.lastread, "ON") == 0) {
+         digitalWrite(LED, HIGH);
+       }
+    if (strcmp((char *)onoffbutton.lastread, "OFF") == 0) {
+         digitalWrite(LED, LOW);
+       }
+
+    }
+  */
+  /*
+    s_dat.ELEC_COND = 25;
+    s_dat.TEMP = 34.5;
+    s_dat.VWC = 10.21;
+  */
+
+  // Elec_Cond.publish(s_dat.ELEC_COND, DEC);
+  /*  if (! Elec_Cond.publish(s_dat.ELEC_COND)) {
+       Serial.println(F("Failed"));
+       } else {
+          Serial.println(F("Got Elec_Cond"));
+       }
+    if (! Temperature.publish((char *) String(s_dat.TEMP).c_str())) {
+       Serial.println(F("Failed"));
+       } else {
+          Serial.println(F("Got Temp"));
+       }
+    if (! VWC.publish((char *) String(s_dat.VWC).c_str())) {
+       Serial.println(F("Failed"));
+       } else {
+          Serial.println(F("Got VWC"));
+       }
+  */
   //delay(7000);
- // Adafruit_MQTT_Subscribe *subscription;
+  // Adafruit_MQTT_Subscribe *subscription;
   Adafruit_MQTT_Subscribe *subscription;
 
+  unsigned long now = millis();
   int x = 0;
-  while(!manager.available() && x < 1000000){
+  while (!manager.available() && (millis() - now < 10000)) {
     x++;
     while ((subscription = mqtt.readSubscription(250))) {
-    if (subscription == &onoffbutton) {
-      Serial.print(F("Got: "));
-      Serial.println((char *)onoffbutton.lastread);
+      if (subscription == &onoffbutton) {
+        Serial.print(F("Got: "));
+        Serial.println((char *)onoffbutton.lastread);
+      }
+      if (strcmp((char *)onoffbutton.lastread, "ON") == 0) {
+        digitalWrite(LED, HIGH);
+      }
+      if (strcmp((char *)onoffbutton.lastread, "OFF") == 0) {
+        digitalWrite(LED, LOW);
+      }
+
     }
-  if (strcmp((char *)onoffbutton.lastread, "ON") == 0) {
-        digitalWrite(LED, HIGH); 
-      }
-  if (strcmp((char *)onoffbutton.lastread, "OFF") == 0) {
-        digitalWrite(LED, LOW); 
-      }
-  
   }
-  }
-  if(manager.available()) {   
+  if (manager.available()) {
     uint8_t len = sizeof(buf);
     uint8_t from;
     memset(buf, '\0', RH_RF95_MAX_MESSAGE_LEN);
@@ -176,24 +195,51 @@ void loop() {
       Serial.println((char*)buf);
       bndl.send(Serial);
       Serial.println("");
+
+
+      //----------------------------------------
+      //------ Pack and send instructions ------
+      //----------------------------------------
+      char inst_mess[MSG_SIZE];
+      memset(inst_mess, '\0', MSG_SIZE);
+
+      float inst_VWC = 20.1;
+      int32_t inst_timer = 5000;
+
+      inst_bndl.empty();
+
+      inst_bndl.add(IDString "/VWC_Inst").add((float)inst_VWC);
+      inst_bndl.add(IDString "/Time_Inst").add((int32_t) inst_timer);
+      inst_bndl.add(IDString "/Mode_Inst").add((int32_t) 0);
+
+      get_OSC_string(&inst_bndl, inst_mess);
+
+      if (manager.sendtoWait((uint8_t*)inst_mess, strlen(inst_mess), RELAY_ADDRESS)) {
+        Serial.println("Instructions sent.");
+      } else {
+        Serial.println("Instruction sending failed -- TODO/Resend?");
+      }
+      //----------------------------------------
+      //------ End of instruction passing ------
+      //----------------------------------------
     }
-  } 
+  }
   //Publish Info to Adafruit.io
   if (! Elec_Cond.publish(s_dat.ELEC_COND)) {
-     Serial.println(F("Failed"));
-     } else {
-        Serial.println(F("Got Elec_Cond"));
-     }
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("Got Elec_Cond"));
+  }
   if (! Temperature.publish((char *) String(s_dat.TEMP).c_str())) {
-     Serial.println(F("Failed"));
-     } else {
-        Serial.println(F("Got Temp"));
-     }
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("Got Temp"));
+  }
   if (! VWC.publish((char *) String(s_dat.VWC).c_str())) {
-     Serial.println(F("Failed"));
-     } else {
-        Serial.println(F("Got VWC"));
-     }
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("Got VWC"));
+  }
 }
 void MQTT_connect() {
   int8_t ret;
@@ -206,10 +252,10 @@ void MQTT_connect() {
   Serial.print("Connecting to MQTT... ");
 
   while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-       Serial.println(mqtt.connectErrorString(ret));
-       Serial.println("Retrying MQTT connection in 5 seconds...");
-       mqtt.disconnect();
-       delay(5000);  // wait 5 seconds
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
   }
   Serial.println("MQTT Connected!");
 }
--- src/relay_device/relay_device.ino
+++ src/relay_device/relay_device.ino
@@ -9,7 +9,7 @@
 #define VALVE_PIN_ROT_CLOSE  10 //Far relay
 
 //Pins for data.
-#define DATAPIN 12         
+#define DATAPIN 12
 #define SENSOR_ADDRESS '?' //Needs to change if running multiple sensors on single data-pin.
 
 //M0 LoRa radio pins.
@@ -45,6 +45,13 @@
 // Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 915.0
 
+typedef struct {
+  boolean valid;
+  float inst_vwc;
+  unsigned long inst_time;
+  int inst_mode;
+} Trigger_Vals;
+
 //Setup Pin-read
 SDI12 mySDI12(DATAPIN);
 
@@ -53,18 +60,20 @@ RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 RHReliableDatagram manager(rf95, RELAY_ADDRESS);
 
+uint8_t inst_buf[RH_RF95_MAX_MESSAGE_LEN];
+
 void setup() {
   digitalWrite(RFM95_RST, LOW);
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
-  
+
   //Setup pins for Valve-control
   pinMode(VALVE_PIN_ROT_OPEN, OUTPUT);
   pinMode(VALVE_PIN_ROT_CLOSE, OUTPUT);
-  
-  
-  while(!Serial); //Comment or remove when not tethering to PC.
+
+
+  while (!Serial); //Comment or remove when not tethering to PC.
   Serial.begin(9600);
   mySDI12.begin(); //Init SDI12 object.
   delay(2000);
@@ -95,7 +104,7 @@ void setup() {
 String sdiResponse = "";
 String resp_buf = "";
 String myCommand = "";
-char delimit[]="+-"; // Delimiters for responce parsing.
+char delimit[] = "+-"; // Delimiters for responce parsing.
 float VWC, temp;
 int elec;
 
@@ -104,11 +113,11 @@ int count = 0; //Testing loop counter.
 OSCBundle bndl;
 
 void loop() {
-  
-//  Serial.print("freeMemory()=");
-//  Serial.println(freeMemory());
-//
-//  delay(1000);
+
+  //  Serial.print("freeMemory()=");
+  //  Serial.println(freeMemory());
+  //
+  //  delay(1000);
 
   Serial.print("Beginning run: #"); Serial.println(++count);
 
@@ -116,11 +125,11 @@ void loop() {
   resp_buf = "";
   myCommand = "";
 
-//-----------------------------
-//----- Begin sensor read -----
-//-----------------------------
+  //-----------------------------
+  //----- Begin sensor read -----
+  //-----------------------------
 
-//first command to take a measurement
+  //first command to take a measurement
   myCommand = String(SENSOR_ADDRESS) + "M!";
   Serial.println(myCommand);     // echo command to terminal
 
@@ -145,7 +154,7 @@ void loop() {
   temp = 99999.9;
   elec = 99999;
 
-// next command to request data from last measurement
+  // next command to request data from last measurement
   myCommand = String(SENSOR_ADDRESS) + "D0!";
   Serial.println(myCommand);  // echo command to terminal
 
@@ -165,45 +174,45 @@ void loop() {
     char * pch;
     char * buf = strdup(sdiResponse.c_str());
     pch = strtok (buf, delimit);// Burn sensor number.
-    
+
     pch = strtok (NULL, delimit);  // VWC value
     VWC = atof(pch);
-    
+
     pch = strtok (NULL, delimit);  // Temp value
     temp = atof(pch);
-        
+
     pch = strtok (NULL, delimit);  // ElecCond value
     elec = atoi(pch);
   }
   Serial.print("VWC: "); Serial.print(VWC);
   Serial.print(" Temp: "); Serial.print(temp);
   Serial.print(" ElecCond: "); Serial.println(elec);
-  
+
   mySDI12.clearBuffer();
-//-----------------------------
-//------ End sensor read ------
-//-----------------------------
+  //-----------------------------
+  //------ End sensor read ------
+  //-----------------------------
 
 
 
   //------ Package read data ------
 
   bndl.empty();
-  
+
   bndl.add(IDString "/VWC").add((float)VWC);
   Serial.print("Add VWC ");
   bndl.add(IDString "/temp").add((float)temp);
   Serial.print("Add Temp ");
   bndl.add(IDString "/ElecCond").add((int32_t)elec);
   Serial.println("Add EC");
-  
+
   char message[MSG_SIZE];
 
   memset(message, '\0', MSG_SIZE);
-  
+
   get_OSC_string(&bndl, message);
 
-  //Serial.println(freeMemory());  
+  //Serial.println(freeMemory());
 
   Serial.println(message);
   Serial.print("Message length: ");
@@ -212,27 +221,42 @@ void loop() {
   Serial.println(RH_RF95_MAX_MESSAGE_LEN);
 
   delay(2000);
-  
+
   Serial.print("Sending...");
-  if (manager.sendtoWait((uint8_t*)message, strlen(message), HUB_ADDRESS)){
+  if (manager.sendtoWait((uint8_t*)message, strlen(message), HUB_ADDRESS)) {
     Serial.println("ok, listening for reply instructions.");
 
-    
+    unsigned long now = millis();
+    while (!manager.available() && (millis() - now < 1000)) {}
+
+    if (manager.available()) {
+      uint8_t len = sizeof(inst_buf);
+      uint8_t from;
+      memset(inst_buf, '\0', RH_RF95_MAX_MESSAGE_LEN);
+      if (manager.recvfromAck(inst_buf, &len, &from)) {
+        OSCBundle inst_bndl;
+        get_OSC_bundle((char*)inst_buf, &inst_bndl);
+        Serial.println((char*)inst_buf);
+        inst_bndl.send(Serial);
+        Serial.println("");
+      }
+    }
+
   } else {
     Serial.println("failed");
   }
-  
-//  Serial.print("freeMemory()=");
-//  Serial.println(freeMemory());
-//
-//  delay(1000);
+
+  //  Serial.print("freeMemory()=");
+  //  Serial.println(freeMemory());
+  //
+  //  delay(1000);
 
 
   Serial.println("----- END RUN -----");
   Serial.println();
   Serial.println();
-  
+
   delay(WAIT * SECOND);
 
-  
+
 }
