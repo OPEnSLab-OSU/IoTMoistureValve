f644e9b954126d3c9e20bf4aaf03b47a9890bc5c
CelticChristoph <celticchristoph@live.com>
2018-06-12 22:59:38 -0700

    Cleaned and prepped for merge with upstream.

--- practice_sketches/rtc_timekeeper/serial_utils.ino
+++
@@ -1,7 +0,0 @@
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-
-void serial_buffer_clear(){
-  while(Serial.available() > 0) {
-    char t = Serial.read();
-  }
-}   
